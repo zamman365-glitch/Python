ab2a9a209f861ecd28830bc7d6b89dcbb14a3639
zamman <zamman365@gmail.com>
2026-05-26 18:12:08 +0530

    fisrt commit

--- StaticandClassMethods.py
+++ StaticandClassMethods.py
@@ -51,3 +51,14 @@ SharmaVishnu.menu()
 # with object
 new_market = SharmaVishnu()
 new_market.menu()"""
+class Animal:
+    name = "Dog" #Class Attribute
+
+    #Instance(Object) can never change your class attributes
+    @classmethod
+    def pookie(cls, new): #self -> Object
+        cls.name = new
+        print(cls.name)
+
+obj = Animal()
+obj.pookie("cat")
